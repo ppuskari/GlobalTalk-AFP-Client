165b51fe067118f8d714a5830575fbf2b4eb5914
ppuskari <ppuskari@hotmail.com>
2026-09-04 05:08:59 -0500

    Implement libc-backed readline shim for Jessie batch build

--- legacy/legacy_compat.c
+++ legacy/legacy_compat.c
@@ -1,5 +1,7 @@
 #include "legacy_compat.h"
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 size_t strlcpy(char *dst, const char *src, size_t dstsize)
@@ -13,4 +15,33 @@ size_t strlcpy(char *dst, const char *src, size_t dstsize)
     }
 
     return srclen;
+}
+
+char *readline(const char *prompt)
+{
+    char buffer[4096];
+    char *line;
+    size_t len;
+
+    if (prompt && *prompt) {
+        fputs(prompt, stdout);
+        fflush(stdout);
+    }
+
+    if (!fgets(buffer, sizeof(buffer), stdin)) {
+        return NULL;
+    }
+
+    len = strlen(buffer);
+    if (len > 0 && buffer[len - 1] == '\n') {
+        buffer[--len] = '\0';
+    }
+
+    line = malloc(len + 1);
+    if (!line) {
+        return NULL;
+    }
+
+    memcpy(line, buffer, len + 1);
+    return line;
 }
